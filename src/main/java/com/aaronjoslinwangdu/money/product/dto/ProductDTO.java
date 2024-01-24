5f1949ba4b2e37112e08caa7f208dbbe81ef96ea
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2024-01-23 21:04:38 -0500

    Add mapper for Product and annotate DTO

--- src/main/java/com/aaronjoslinwangdu/money/product/dto/ProductDTO.java
+++ src/main/java/com/aaronjoslinwangdu/money/product/dto/ProductDTO.java
@@ -3,8 +3,18 @@ package com.aaronjoslinwangdu.money.product.dto;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import com.aaronjoslinwangdu.money.account.dto.AccountDTO;
 import com.aaronjoslinwangdu.money.common.dto.AbstractDTOAttributes;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO extends AbstractDTOAttributes implements Serializable {
 
 	private static final long serialVersionUID = 4L;
@@ -14,7 +24,7 @@ public class ProductDTO extends AbstractDTOAttributes implements Serializable {
 	private Long productType;
 	
 	private String name;
-	
+
 	private String description;
 	
 	private BigDecimal price;
---
+++ src/main/java/com/aaronjoslinwangdu/money/product/mapper/ProductMapper.java
@@ -0,0 +1,41 @@
+package com.aaronjoslinwangdu.money.product.mapper;
+
+import java.io.Serializable;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.aaronjoslinwangdu.money.account.domain.Account;
+import com.aaronjoslinwangdu.money.account.dto.AccountDTO;
+import com.aaronjoslinwangdu.money.product.domain.Product;
+import com.aaronjoslinwangdu.money.product.dto.ProductDTO;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+	
+	/**
+	 * Convert Product Domain Object to DTO
+	 * 
+	 * @param account
+	 * @return
+	 */
+	@Mappings({
+		@Mapping(source = "dbky", target = "id")
+	})
+	ProductDTO convertSalesOrderDomainToDto(Product product);
+	
+	
+	/**
+	 * Updates fields from Product DTO to Entity
+	 * 
+	 * @param request
+	 * @param account
+	 */
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+	void updateValuesToAccountEntity(ProductDTO request, @MappingTarget Product product);
+	
+}
